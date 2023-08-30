f948e312b59c3cfa8691f0668b666f43c55abdb1
Ike <dylanraspino264@gmail.com>
2023-08-30 00:49:09 +0000

    updated views

--- drinks/api/urls.py
+++ drinks/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.drinks)
+    path('drinks/', views.drink_list),
+    path('drinks/<int:id>', views.drink_detail),
 ]
--- drinks/api/views.py
+++ drinks/api/views.py
@@ -1,10 +1,48 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .serializers import DrinkSerializer
+from .models import Drink
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
-@api_view('GET', 'POST')
+@api_view(['GET', 'POST'])
 def drink_list(request):
+
     if request.method == 'GET':
-        pass
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id):
+
+
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        serializer = DrinkSerializer(drink, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
