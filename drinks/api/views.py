1bfb58a658d471c74dbb0455e3a525eb425bede5
Ike <dylanraspino264@gmail.com>
2023-08-30 00:24:59 +0000

    changed function name

--- drinks/api/views.py
+++ drinks/api/views.py
@@ -5,6 +5,6 @@ from .serializers import DrinkSerializer
 # Create your views here.
 
 @api_view('GET', 'POST')
-def drinks(request):
+def drink_list(request):
     if request.method == 'GET':
         pass
